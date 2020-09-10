d99b8fe7cc131694fd47823a70e6f8502da91fe6
josh <joshua.s798@stu.nebo.edu>
2020-09-10 09:03:07 -0600

    Added player input

---
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float turnSpeed;
+    public float horizontalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        // Turn the player
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
