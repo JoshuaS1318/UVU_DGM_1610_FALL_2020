890e90db167361e64c65f6640343fe6ab8dfbdc7
josh <joshua.s798@stu.nebo.edu>
2020-09-16 09:06:35 -0600

    finished challenge 1

---
+++ Prototype1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    private float spinSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin the propeller by spinSpeed
+        transform.Rotate(Vector3.forward * Time.deltaTime * spinSpeed);
+    }
+}
