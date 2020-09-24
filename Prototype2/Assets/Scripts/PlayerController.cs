d128d8830f3ad70950c3de73939cddd5a9a1e304
josh <joshua.s798@stu.nebo.edu>
2020-09-24 09:09:49 -0600

    Started prototype2

---
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public GameObject projectilePrefab;
+    private float xRange = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the player on the horizontal axis when horizontal input is received
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        // Keep the player in the bounds
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        } else if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        // Take player spacebar input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
