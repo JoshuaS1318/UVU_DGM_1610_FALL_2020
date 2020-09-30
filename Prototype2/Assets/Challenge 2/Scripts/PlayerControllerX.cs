1e7b4e2aca84d5309a7e16aef5f3fd70c2bfe7c4
josh <joshua.s798@stu.nebo.edu>
2020-09-30 09:06:25 -0600

    Completed Prototype2

---
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool cooldown = false;
+    private float cooldownTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !cooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // Start cooldown
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    // Cooldown for cooldownTime seconds between being able to spawn dogs
+    IEnumerator Cooldown()
+    {
+        cooldown = !cooldown;
+        yield return new WaitForSeconds(cooldownTime);
+        cooldown = !cooldown;
+    }
+}
---
+++ Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 3.0f;
+    private float lowerRandomTime = 0.5f;
+    private float higherRandomTime = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        // Wait for random timer to spawn animal
+        StartCoroutine(RandomTimer());
+    }
+
+    // Starts a timer for a random amount of time to wait before spawning an animal
+    IEnumerator RandomTimer()
+    {
+
+        // Wait for random amount of time
+        yield return new WaitForSeconds(Random.Range(lowerRandomTime, higherRandomTime));
+
+        // Random index for what ball to use
+        int ballIndex = Random.Range(0, 3);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+}
