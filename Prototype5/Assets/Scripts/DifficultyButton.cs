18df92d3f128fe03741c10d2acf590ac6eff8e61
josh <joshua.s798@stu.nebo.edu>
2020-11-12 09:10:41 -0700

    worked on challenge5

--- Prototype5/Assets/Scripts/DifficultyButton.cs
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -5,28 +5,29 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
+    // Higher number causes faster spawning
     public int difficulty;
 
+    // Instantiate variables for GameManager and button
     private Button button;
     private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get a reference to the GameManager script
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        // Get a reference to the button
         button = GetComponent<Button>();
+        // Add an event listener to the button to start the game with the buttons difficulty
         button.onClick.AddListener(SetDifficulty);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// Set the difficulty multiplier of the game
+    /// </summary>
     void SetDifficulty()
     {
-        Debug.Log(gameObject.name + " was clicked");
         gameManager.StartGame(difficulty);
     }
 }
